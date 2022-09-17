2f361debb61bbe357994d5d900f43bb0e9d74971
fiokee <smleffiong@gmail.com>
2022-09-17 02:08:34 -0700

    Hero section of the landing page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Destinations from "./components/Destinations";
 import Header from "./components/landing-page/Header";
 
 
@@ -8,6 +9,7 @@ function App() {
     <div>
      
       <Header/>
+      <Destinations/>
     </div>
   );
 }
--- src/components/landing-page/NavBar.jsx
+++ src/components/landing-page/NavBar.jsx
@@ -1,22 +1,23 @@
 import React, {useState} from 'react'
 import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 import { Link } from 'react-router-dom';
+import SignIn from './SignIn';
 
 const NavBar = () => {
 
     const [openNav, setOpenNav]=useState(false)
   return (
-    <nav className='bg-white'>
+    <nav className='bg-black text-white'>
       <div className='flex items-center font-medium justify-around'>
         <div className='z-50 p-5 md:w-auto w-full'>
-            <h1 className='md:cursor-pointer h-9'>Logo Here</h1>
+            <h1 className='md:cursor-pointer h-9 border-5% text-yellow'>Logo Here</h1>
         </div>
         <div className='z-50 text-3xl p-4 md:hidden' onClick={()=> setOpenNav(!openNav)}>
-            {!openNav ? <AiOutlineMenu/>:  <AiOutlineClose/> }
+            {!openNav ? <AiOutlineMenu/>:  <AiOutlineClose className='text-black'/> }
             </div>
             <ul className='md:flex hidden capitalize items-center gap-8 font-[Poppins]'>
                 <li>
-                    <Link to="/" className='py-7 px-3'>
+                    <Link to="/" className='py-7 px-3 text-F3DE1B'>
                         Explore
                     </Link>
                 </li>
@@ -31,22 +32,20 @@ const NavBar = () => {
                     </Link>
                 </li>
                 <li>
-                    <Link to="tourists" className='py-7 px-3 inline-block'>
+                <Link to="tourists" className='py-7 px-3 inline-block'>
                         tourists
-                    </Link>
+                </Link>
                 </li>
-                <li>
-                    <Link to="signin" className='py-7 px-3 inline-block'>
-                        signin
-                    </Link>
-                </li>
-            </ul>
-            
+               </ul>
+               <div className='md:block hidden'>
+                 <SignIn/>
+               </div>
+                       
             {/*Mobile Navigation */}
             
-            <ul className= {`md:hidden bg-white absolute w-full h-full bottom-10 py-20 pl-4 duration-500 ${openNav ? "left-0" : "left-[-100%]"}`}>
+            <ul className= {`md:hidden bg-white absolute w-full h-full bottom-10 py-20 pl-4 duration-500 ${openNav ? "left-0" : "left-[-100%]"} text-gray-dark`}>
                 <li>
-                    <Link to="/" className='py-6 px-3 inline-block'>
+                    <Link to="/" className='py-8 px-3 inline-block'>
                         Explore
                     </Link>
                 </li>
@@ -65,14 +64,10 @@ const NavBar = () => {
                         tourists
                     </Link>
                 </li>
-                <li>
-                    <Link to="signin" className='py-6 px-3 inline-block'>
-                        signin
-                    </Link>
-                </li>
-      </ul>
-      </div>
-    </nav>
+              <SignIn/>
+              </ul>
+             </div>
+            </nav>
   )
 }
 
