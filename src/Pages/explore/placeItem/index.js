372136e7226faefaae5a5d3a58352aa435c122ac
fiokee <smleffiong@gmail.com>
2022-11-02 00:31:16 +0100

    testing

--- src/Pages/explore/placeItem/index.js
+++ src/Pages/explore/placeItem/index.js
@@ -15,7 +15,7 @@ const PlaceItem = () => {
     <div className={styles.main}>
       <div className={styles.postHead}>
 
-        <div className={styles.image}><img src='../../travel.jpg'/></div>
+        <div className={styles.image}><img src='https://images.pexels.com/photos/326058/pexels-photo-326058.jpeg?auto=compress&cs=tinysrgb&w=600'/></div>
 
         <div className={styles.tittle}>Jeson Derulo</div>
 
