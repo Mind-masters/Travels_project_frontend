d2633bb79de5935e35274d88e5d740bef79b2c7f
dominykas <bubnys.it@gmail.com>
2022-11-04 17:32:52 +0300

    creating yourTrip page

---
+++ src/Pages/yourTrip/Trips/matchesYourInterests.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MatchesYourInterests = () => {
+  return (
+    <div>MatchesYourInterests</div>
+  )
+}
+
+export default MatchesYourInterests
---
+++ src/components/utils/fetchUserPlaces.js
@@ -0,0 +1,45 @@
+const DUMMY_DATA = [
+    {
+        title: "first place",
+        description: "adding your first place!",
+        image: "https://www.makalius.lt/wp-content/post-images/25713815-post/sauni-isvyka-i-zakopane-kelione-autobusu-2-nakvynes-viesbutyje-su-pusryciais-bei-ekskursijos-145-7025-540x380.png",
+        address: "New :)"
+    },
+
+    {
+        title: "second place",
+        description: "adding your first place!",
+        image: "https://www.makalius.lt/wp-content/post-images/25713815-post/sauni-isvyka-i-zakopane-kelione-autobusu-2-nakvynes-viesbutyje-su-pusryciais-bei-ekskursijos-145-7025-540x380.png",
+        address: "New :)"
+    },
+
+    {
+        title: "third place",
+        description: "adding your first place!",
+        image: "https://www.makalius.lt/wp-content/post-images/25713815-post/sauni-isvyka-i-zakopane-kelione-autobusu-2-nakvynes-viesbutyje-su-pusryciais-bei-ekskursijos-145-7025-540x380.png",
+        address: "New :)"
+    },
+
+    {
+        title: "fourth place",
+        description: "adding your first place!",
+        image: "https://www.makalius.lt/wp-content/post-images/25713815-post/sauni-isvyka-i-zakopane-kelione-autobusu-2-nakvynes-viesbutyje-su-pusryciais-bei-ekskursijos-145-7025-540x380.png",
+        address: "New :)"
+    },
+
+    {
+        title: "fifth place",
+        description: "adding your first place!",
+        image: "https://www.makalius.lt/wp-content/post-images/25713815-post/sauni-isvyka-i-zakopane-kelione-autobusu-2-nakvynes-viesbutyje-su-pusryciais-bei-ekskursijos-145-7025-540x380.png",
+        address: "New :)"
+    },
+
+    {
+        title: "sixth place",
+        description: "adding your first place!",
+        image: "https://www.makalius.lt/wp-content/post-images/25713815-post/sauni-isvyka-i-zakopane-kelione-autobusu-2-nakvynes-viesbutyje-su-pusryciais-bei-ekskursijos-145-7025-540x380.png",
+        address: "New :)"
+    }
+]
+
+export default DUMMY_DATA;
