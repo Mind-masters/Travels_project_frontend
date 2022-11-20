0296766f5f86d22f264f92315e9e9d1d5b7b234a
fiokee <smleffiong@gmail.com>
2022-11-20 18:09:58 +0100

    profile page

--- src/Pages/profile/index.js
+++ src/Pages/profile/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useContext } from 'react'
 import Card from '../../components/shared/UI/Card';
 import {AuthContext} from "../../contextAPI/AuthContext";
-import styles from "./profile.module.css"
+import Styles from "./profile.module.css"
 import profileImage from '../../assets/Profile Pic.png';
 import placeImage from '../../assets/Rectangle 104.png';
 import arrowImage from '../../assets/arrow.png'
@@ -11,41 +11,39 @@ import profileFlag from '../../assets/flag.png';
 import profileMail from '../../assets/mail.png';
 
 const Profile = () => {
-
-
     return (
         <Card>
-            <div className={styles.container}>
-        <div className={styles.wrapper}>
+            <div className={Styles.container}>
+        <div className={Styles.wrapper}>
 
-            <img className={styles.profile_image} src={profileImage} alt=""></img>
-            <h1 className={styles.profile_name}>Johnny Depp</h1>
+            <img className={Styles.profile_image} src={profileImage} alt=""></img>
+            <h1 className={Styles.profile_name}>Johnny Depp</h1>
         
-        <div className={styles.profile_place}>
-            <h2>Places <span className={styles.number_place}>3</span></h2>
-            <span className={styles.place_image}><img src={placeImage} alt=""></img></span>
+        <div className={Styles.profile_place}>
+            <h2>Places <span className={Styles.number_place}>3</span></h2>
+            <span className={Styles.place_image}><img src={placeImage} alt=""></img></span>
         </div>
-        <div className={styles.profile_setting}>
-            <span className={styles.profile_setting}>...</span>
+        <div className={Styles.profile_setting}>
+            <span className={Styles.profile_setting}>...</span>
         </div>
         </div>
-        <div className={styles.profile_interest}>
-            <h1 className={styles.hobby}>Hobbeis</h1>
-            <div className={styles.profile_hobby}>
+        <div className={Styles.profile_interest}>
+            <h1 className={Styles.hobby}>Hobbeis</h1>
+            <div className={Styles.profile_hobby}>
 
-        <p className={styles.interest}>Hills</p>
-        <p className={styles.interest}>Hills</p>
+        <p className={Styles.interest}>Hills</p>
+        <p className={Styles.interest}>Hills</p>
             </div>
         </div>
             <h1>About</h1>
-            <div className={styles.box}>
-                <div className={styles.details}>
+            <div className={Styles.box}>
+                <div className={Styles.details}>
 
-                <p className={styles.details_name}><img src={profileName} alt=""/>Johnny Depp</p>
+                <p className={Styles.details_name}><img src={profileName} alt=""/>Johnny Depp</p>
                 <p><img src={profileFlag}/>Sweden</p>
                 <p><img src={profileMail}/>Johny.test@gmail.com</p>
                 </div>
-                <div className={styles.arrow} >
+                <div className={Styles.arrow} >
 
                 <img src={arrowImage} alt=""></img>
                 </div>
