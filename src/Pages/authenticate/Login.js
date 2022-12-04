61497128ecdfa44b677ce8abc5e460dd5937dab8
dominykas <bubnys.it@gmail.com>
2022-12-04 17:54:15 +0300

    updating url link

--- src/Pages/authenticate/Login.js
+++ src/Pages/authenticate/Login.js
@@ -32,7 +32,7 @@ const Login = () => {
 
         setIsLoading(true);
         
-        const Req = await fetch(`http://localhost:5000/api/v1/client/auth/login`, {
+        const Req = await fetch(`https://mind-masters-server.onrender.com/api/v1/client/auth/login`, {
           method: "POST",
           headers: {
             "Content-Type" : "application/json",
