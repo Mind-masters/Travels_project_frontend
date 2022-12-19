86574a099e4b534f86498756a7df861cc891d109
dominykas <bubnys.it@gmail.com>
2022-12-19 10:16:58 +0300

    initializing avataars component

---
+++ src/components/shared/UI/avataars/Appearance.js
@@ -0,0 +1,44 @@
+export const Data = [
+    {
+        Top: [
+            "NoHair",
+            "EyePatch",
+            "Hat",
+            "Hijab",
+            "Turban",
+            "WinterHat1",
+            "WinterHat2",
+            "WinterHat3",
+            "WinterHat4",
+            "LongHairBigHair",
+            "LongHairBob",
+            "LongHairBun",
+            "LongHairCurly",
+            "LongHairCurvy",
+            "LongHairDreads",
+            "LongHairFrida",
+            "LongHairFro",
+            "LongHairFroBand",
+            "LongHairNotTooLong",
+            "LongHairShavedSides",
+            "LongHairMiaWallace",
+            "LongHairStraight",
+            "LongHairStraight2",
+            "LongHairStraightStrand",
+            "LongHairDreads01",
+            "LongHairDreads02",
+            "LongHairFrizzle",
+            "LongHairShaggyMullet",
+            "LongHairShortCurly",
+            "LongHairShortFlat",
+            "LongHairShortRound",
+            "LongHairShortWaved",
+            "LongHairSides",
+            "LongHairTheCaesar",
+            "LongHairTheCaesarSidePart",
+        ],
+        Accessories: [
+            
+        ]
+    }
+]
