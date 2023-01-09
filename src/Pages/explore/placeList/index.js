11521618ca648f41231fcba43709aafed316f5c8
dominykas <dominykasbubnys@gmail.com>
2023-01-09 14:28:20 +0300

    updating app and fetching realtime data in explore page

--- src/Pages/explore/placeList/index.js
+++ src/Pages/explore/placeList/index.js
@@ -1,19 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from "./index.module.css";
 import PlaceItem from "../placeItem";
+import { fetchAllPlaces } from '../../../components/utils/places/fetchPlaces';
+import { notify } from "../../../components/shared/UI/toast";
+import LoadingSpinner from '../../../components/shared/UI/LoadingSpinner';
 
-const index = () => {
-  return (
-    <div className={styles.container}>
-      <h1 className={styles.container_header}>Explore Destination</h1>
+const PlaceList = () => {
 
-      <div className={styles.list_container}>
-        <PlaceItem />
-        <PlaceItem />
-        <PlaceItem />
-      </div>
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async() => {
+
+      setIsLoading(true)
+      const placesData = await fetchAllPlaces();
+      setIsLoading(false);
+      console.log("data: ", placesData);
+      if(!placesData.status)notify(placesData.message, "error")
+      setData(placesData.data);
+    }
+
+
+    fetchData();
+  }, [])
+
+
+  const ListItemElement = () => (
+    <div className={styles.list_container}>
+      {
+        data.map(item => <PlaceItem item={item} />)
+      }
     </div>
+  )
+
+  return (
+    <>
+      {isLoading && <LoadingSpinner />}
+      {!isLoading && data && 
+        <div className={styles.container}>
+          <h1 className={styles.container_header}>Explore Destination</h1>
+          <ListItemElement />
+        </div>
+      }
+    </>
   )
 }
 
-export default index
+export default PlaceList
