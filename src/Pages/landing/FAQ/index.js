5b34186e3916e77717c174dd95e65d791c93e4e3
fiokee <smleffiong@gmail.com>
2023-05-15 23:14:51 +0100

    fixed bug on landing page FAQ section and added some animation to the buttons using framer motion

--- src/Pages/landing/FAQ/index.js
+++ src/Pages/landing/FAQ/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { motion } from "framer-motion"
 import styles from "./faq.module.css";
 import {DummyData} from "./Data"
 import FaqItem from './item';
--- src/Pages/landing/FAQ/item.js
+++ src/Pages/landing/FAQ/item.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from "./item.module.css";
 import expand_logo from "../../../assets/landing/expand_logo.png";
+import expanded_logo from "../../../assets/landing/expanded_logo.png";
 import { useState } from 'react';
 
 const FaqItem = ({obj}) => {
@@ -8,14 +9,17 @@ const FaqItem = ({obj}) => {
     const [showBody, setShowBody] = useState(false)
 
     const headerClickHandler = () => {
-        setShowBody(!showBody);
+        setShowBody(!showBody);  
     }
 
   return (
     <div onClick={headerClickHandler} className={`${styles.box} ${showBody && styles.active_box}`}>
         <div className={styles.header} >
             <h1>{obj.question}</h1>
-            <img src={expand_logo} alt='' />
+            {
+                showBody ?  <img src={expanded_logo} alt='' /> :  <img src={expand_logo} alt='' />
+            }
+            
         </div>
 
         <div className={`${styles.body} ${showBody && styles.active_body}`}>
