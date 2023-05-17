9a5113e6d5f3ca8eb00989be2ae4fe06a6a6ca4c
dominykas <dominykasbubnys@gmail.com>
2023-05-18 00:52:48 +0300

    too much text in aboutUs section

--- src/Pages/landing/aboutUs/index.js
+++ src/Pages/landing/aboutUs/index.js
@@ -27,7 +27,7 @@ const AboutUs = () => {
                 Welcome to Tripwhoop, the ultimate platform for passionate travelers to connect, share their experiences, and embark on exciting journeys together. We believe that traveling is not just about exploring new places, but also about the connections we make along the way.
 
                 At Tripwhoop, we aim to build a vibrant community of adventurers from around the world. Whether you're a seasoned traveler or just starting your exploration journey, our platform provides a space for you to connect with like-minded individuals who share your love for travel.
-                <br/>
+                {/* <br/>
                 Our web app offers a seamless and user-friendly interface, empowering you to share your travel stories, upload captivating photos, and even mark the exact coordinates of the places you've visited. By doing so, you not only create a virtual map of your adventures but also inspire 
                 and guide others in their own wanderlust-filled pursuits.
                 <br/>
@@ -38,7 +38,7 @@ const AboutUs = () => {
 
                 Join our growing community of travel enthusiasts today and unlock a world of endless possibilities. Together, let's celebrate the joy of exploration, forge lifelong connections, and make every journey an unforgettable experience.
 
-                Start your adventure with Tripwhoop and let the exploration begin!
+                Start your adventure with Tripwhoop and let the exploration begin! */}
                 </p>
             </div>
         </div>
