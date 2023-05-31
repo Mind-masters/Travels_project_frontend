664078e037755a3a840d8f7abf87ec25babaad97
fiokee <smleffiong@gmail.com>
2023-05-31 18:19:47 +0100

    adding functionalities for blog post creating and storing data

--- src/Pages/Blog/FeaturedPost.js
+++ src/Pages/Blog/FeaturedPost.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import  styles from './featuredpost.module.css';
 import bannerimg from '../../assets/Blog/featured-1.jpg';
 import bannerimg2 from '../../assets/Blog/recent-2.jpg';
@@ -7,77 +7,63 @@ import Author1 from '../../assets/Blog/author-1.jpg';
 import Author2 from '../../assets/Blog/author-2.jpg';
 import Author3 from '../../assets/Blog/author-6.jpg';
 import Author4 from '../../assets/Blog/author-4.jpg'
+import axios from 'axios';
+import BlogPostForm from './BlogPostForm';
+
 const FeaturedPost = () => {
+const [posts, setPosts]= useState({})
+
+useEffect(()=>{
+fetchPosts();
+},[])
+
+const fetchPosts = async () => {
+  try {
+    const response = await axios.get('/api/posts');
+    setPosts(response.data);
+  } catch (error) {
+    console.error('Error fetching posts:', error);
+  }
+};
+
+const handlePostCreated = (newPost) => {
+  setPosts([...posts, newPost]);
+};
+
   return (
-    
-  <div class={styles.wrapper}>
-  
-  <div class={styles.card}>
-    
-      <div class={styles.card_banner}>
-        <p class={styles.category_tag}>Technology</p>
-        <img class={styles.banner_img} src={bannerimg} alt=""/>
+    <Fragment>
+  <h2 className={styles.write_post}>Write Post</h2>
+  <div className={styles.wrapper}>
+    {/* <BlogPostForm onPostCreated={handlePostCreated} />
+    {
+      posts.map((post)=>(
+      <div key={post.id}>
+        <h2>{post.title}</h2>
+      </div>
+      ))} */}
+    <div className={styles.card}>
+      <div className={styles.card_banner}>
+        <p className={styles.category_tag}>Technology</p>
+        <img className={styles.banner_img} src={bannerimg} alt=""/>
       </div>
 
-      <div class={styles.card_body}>
-        <p class={styles.blog_hashtag}>#Tech #Code</p>
-        <h2 class={styles.blog_title}>New technology is not good or evil in and of itself</h2>
-        <p class={styles.blog_description}>Lorem ipsum dolor sit amet, consectetur adipisicing</p>
+      <div className={styles.card_body}>
+        <p className={styles.blog_hashtag}>#Tech #Code</p>
+        <h2 className={styles.blog_title}>New technology is not good or evil in and of itself</h2>
+        <p className={styles.blog_description}>Lorem ipsum dolor sit amet, consectetur adipisicing</p>
 
-        <div class={styles.card_profile}>
-          <img class={styles.profile_img} src={Author1} alt=''/>
-          <div class={styles.card_profile_info}>
-            <h3 class={styles.profile_name}>Eleanor Pea</h3>
-            <p class={styles.profile_followers}>5.2k followers</p>
+        <div className={styles.card_profile}>
+          <img className={styles.profile_img} src={Author1} alt='post'/>
+          <div className={styles.card_profile_info}>
+            <h3 className={styles.profile_name}>Eleanor Pea</h3>
+            <p className={styles.profile_followers}>5.2k followers</p>
           </div>
         </div>
       </div>
     </div>
 
-    <div class={styles.card}>
-    
-    <div class={styles.card_banner}>
-      <p class={styles.category_tag}>Design</p>
-      <img class={styles.banner_img} src={bannerimg2} alt=""/>
     </div>
-
-    <div class={styles.card_body}>
-      <p class={styles.blog_hashtag}>#Tech #Design</p>
-      <h2 class={styles.blog_title}>Being unique is better than being perfect</h2>
-      <p class={styles.blog_description}>Lorem ipsum dolor sit amet, consectetur adipisicing</p>
-
-      <div class={styles.card_profile}>
-        <img class={styles.profile_img} src={Author3} alt=''/>
-        <div class={styles.card_profile_info}>
-          <h3 class={styles.profile_name}>Eleanor Pea</h3>
-          <p class={styles.profile_followers}>5.2k followers</p>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class={styles.card}>
-    
-    <div class={styles.card_banner}>
-      <p class={styles.category_tag}>#Lifestyle</p>
-      <img class={styles.banner_img} src={bannerimg3} alt=""/>
-    </div>
-
-    <div class={styles.card_body}>
-      <p class={styles.blog_hashtag}>#Lifestyle #People</p>
-      <h2 class={styles.blog_title}>Creating is a privilege but it’s also a gift</h2>
-      <p class={styles.blog_description}>Lorem ipsum dolor sit amet, consectetur adipisicing</p>
-
-      <div class={styles.card_profile}>
-        <img class={styles.profile_img} src={Author4} alt=''/>
-        <div class={styles.card_profile_info}>
-          <h3 class={styles.profile_name}>Eleanor Pea</h3>
-          <p class={styles.profile_followers}>5.2k followers</p>
-        </div>
-      </div>
-    </div>
-  </div>
-
-    </div>
+    </Fragment>
   )
 }
 
--- src/Pages/Blog/index.js
+++ src/Pages/Blog/index.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react'
 import HeroSection from './HeroSection';
 import styles from './blog.module.css'
 import FeaturedPost from './FeaturedPost';
+import BlogPostForm from './BlogPostForm';
 
 const Blog = ()=>{
 return (
     <div className={styles.container}>
     <HeroSection/>
     <FeaturedPost/>
+    <BlogPostForm/>
     </div>
 )
 }
