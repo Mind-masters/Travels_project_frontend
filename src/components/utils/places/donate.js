013291ff0a286fefb72c76b2b0a664fe53415142
dominykas <dominykasbubnys@gmail.com>
2023-11-09 13:34:25 +0300

    fixing donate url

--- src/components/utils/places/donate.js
+++ src/components/utils/places/donate.js
@@ -2,7 +2,7 @@ import { FetchAPI_template } from "../FetchAPI_template";
 
 export const Donate = async(data, token) => {
 
-    const url = "https://mind-master-backend-production.up.railway.app/api/v1/user/places/new";
+    const url = "https://mind-master-backend-production.up.railway.app/api/v1/user/donate";
     // const url = "http://localhost:5000/api/v1/user/donate"
 
 
